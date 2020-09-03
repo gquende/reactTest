48ce227d1369e91e090ba22c0c71a4add0c905f6
Geraldo Quende <adolfoquende@hotmail.com>
2020-09-03 11:14:42 +0100

    How to create components

---
+++ learning/src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+//Statless components
+const Header = () => <header id="main-header">GQUENDE</header>;
+//Pode se usar esta forma para retornar um componente
+
+export default Header;
+
+
+
+
+
+
+
+
+//This form is too avalaible, so it is used to when we have states
+/*
+class Header extends Component{
+
+render(){
+
+    return <h1>Hello</h1>
+}
+
+}*/
