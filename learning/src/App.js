63c53570c21139032ac5c24c0a5a656a2843fb82
Geraldo Quende <adolfoquende@hotmail.com>
2020-09-15 12:06:17 +0100

    Return in the Game

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+//import logo from './logo.svg';
+//import './App.css';
+//import Header from './components/Header';
+import './components/styles/global.css'
 import Teste from './components/Teste';
+import Header from './components/pages/Header';
 
 
 function App() {
   return (
     <div className="App">
-
       <Header></Header>
-      <Teste></Teste>
-
     </div>
   );
 }
---
+++ learning/src/components/pages/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css'
+
+
+
+function Header() {
+
+
+    return (<header className="head">
+
+        <h1>Adolfo Quende</h1>
+
+    </header>
+    );
+}
+export default Header;
